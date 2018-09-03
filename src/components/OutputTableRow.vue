7580dfb9c62f2faee897363b89e8cd27d01f90f1
Masahiro Hayashi <mhayashi@mshackers.co.jp>
2018-09-03 10:38:55 +0900

    有効桁数3桁で表示  #1

--- src/components/OutputTableRow.vue
+++ src/components/OutputTableRow.vue
@@ -9,14 +9,29 @@ code {
 <template>
     <tr>
         <th><code>{{result.label}}</code> : {{result.name}}</th>
-        <td :class="{ 'table-danger': result.n === minN }">{{Math.round(result.n)}}</td>
-        <td :class="{ 'table-danger': result.nMm === minNMm }">{{Math.round(result.nMm)}}</td>
+        <td :class="{ 'table-danger': result.n === minN }">{{n}}</td>
+        <td :class="{ 'table-danger': result.nMm === minNMm }">{{nMm}}</td>
     </tr>
 </template>
 
 <script>
+function round(i) {
+  if (i < 0.1 || 100000 <= i) {
+    return i.toExponential(2);
+  }
+  return parseFloat(i.toPrecision(3));
+}
+
 export default {
   props: ["result", "minN", "minNMm"],
+  computed: {
+    n() {
+      return round(this.result.n);
+    },
+    nMm() {
+      return round(this.result.nMm);
+    },
+  },
 };
 </script>
 
